ff4d5a9e45753a8241e93edaabe1979f084b3328
mtepfer <mtepfer1@gmail.com>
2018-03-06 03:53:59 -0500

    Using new version of hunter

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -20,24 +20,25 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.]]
 
-function ( download_and_include SRC DEST )
-    if ( NOT EXISTS ${DEST} )
-        message ( STATUS "Downloading ${DEST} from ${SRC}" )
-        file ( DOWNLOAD "${SRC}" "${DEST}" )
+macro ( download_and_include SRC )
+    get_filename_component ( download_and_include_FILE_NAME ${SRC} NAME )
+    string ( CONCAT download_and_include_DEST ${CMAKE_CURRENT_BINARY_DIR}/downloaded-cmake-modules/ ${download_and_include_FILE_NAME} )
+    if ( NOT EXISTS ${download_and_include_DEST} )
+        message ( STATUS " Downloading cmake module from ${SRC}" )
+        file ( DOWNLOAD "${SRC}" "${download_and_include_DEST}" )
     else ()
-        message ( STATUS "Using downloaded file ${DEST}" )
+        message ( STATUS "Using cmake module downloaded from ${SRC}" )
     endif ()
-    if ( NOT EXISTS ${DEST} )
+    if ( NOT EXISTS ${download_and_include_DEST} )
         message ( FATAL_ERROR "File ${DEST} is missing" )
     endif ()
-    include ( ${DEST} )
-endfunction ()
+    include ( ${download_and_include_DEST} )
+endmacro ()
 
 macro ( start_hunter )
-    download_and_include ( "https://raw.githubusercontent.com/hunter-packages/gate/master/cmake/HunterGate.cmake"
-                           "${CMAKE_CURRENT_BINARY_DIR}/HunterGate.cmake" )
+    download_and_include ( "https://raw.githubusercontent.com/hunter-packages/gate/master/cmake/HunterGate.cmake" )
     huntergate (
-            URL "https://github.com/ruslo/hunter/archive/v0.19.228.tar.gz"
-            SHA1 "5f8bde642d44d2ef652c917fa6b36dbc5f693d51"
+            URL "https://github.com/ruslo/hunter/archive/v0.20.14.tar.gz"
+            SHA1 "18cdbf5c47abdda437c73bf5437b7c3c65afe47c "
     )
 endmacro ()
